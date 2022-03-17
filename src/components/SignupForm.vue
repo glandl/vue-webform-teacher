367da4d07f8d7746796e303382ab89d4acc7263a
glandl <glandl@eurogym.info>
2022-03-17 13:16:44 +0100

    1 file modified

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,23 +1,86 @@
 <template>
   <form>
     <label>Email</label>
-    <input type="email" required v-model="email">
+    <input type="email" required v-model="email" />
     <label>Password</label>
-    <input type="password" required v-model="password">
+    <input type="password" required v-model="password" />
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-      }
-    }
-
-  }
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
 </script>
 <style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1xp solid #ddd;
+  color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
 </style>
